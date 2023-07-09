f55253246c8c0023da0b803aa17e5eeaead0106c
Anthony Eduardo <105107191+AnthonyEduardo5@users.noreply.github.com>
2023-07-09 17:03:52 -0300

    Criação do componente modal para cadastro de marcas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -2,7 +2,6 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-
                 <!-- Início do card de busca-->
                 <card-component titulo="Busca de marcas">
                     <template v-slot:conteudo>
@@ -33,13 +32,34 @@
                     </template>
 
                     <template v-slot:rodape>
-                        <button type="button" class="btn btn-primary btn-sm float-right">Adicionar</button>
+                        <button type="button" class="btn btn-primary btn-sm float-right"  data-toggle="modal" data-target="#modalMarca">Adicionar</button>
                     </template>
                 </card-component>
                 <!-- Fim do card de listagem de marcas -->
 
             </div>
         </div>
+
+        <!-- Modal -->
+        <modal-component id="modalMarca" titulo="'Adicionar marca'">
+            <template v-slot:conteudo>
+                <div class="form-group">
+                    <input-container-component titulo="Nome da marca" id="novoNome" idHelp="novoNomeHelp" texto-ajuda="informe o nome da marca">
+                        <input type="type" class="form-control" id="novoNome" aria-describedby="novoNomeHelp" placeholder="Nome da marca">
+                    </input-container-component>
+                </div>
+                
+                <div class="form-group">
+                    <input-container-component titulo="Imagem" id="novoImagem" idHelp="novoImagemHelp" texto-ajuda="Selecione uma imagem no formato PNG">
+                        <input type="file" class="form-control-file" id="novoImagem" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem">
+                    </input-container-component>
+                </div>
+            </template>
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary">Salvar</button>
+            </template>
+        </modal-component>
     </div>
 </template>
 
